9a34699715ce64f4ca27a8b75a69ab2820822d34
AdrianXtreme <71661344+AdrianXtreme@users.noreply.github.com>
2020-10-15 15:59:50 +0200

    initial commit
    initial commit added

---
+++ calculadoramadeinChina/calculadoramadeinChina/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace calculadoramadeinChina
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

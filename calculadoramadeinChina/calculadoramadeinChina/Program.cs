8e1f04f6c3530a564062e1cc035d7c754311ac50
AdrianXtreme <71661344+AdrianXtreme@users.noreply.github.com>
2020-10-19 19:01:39 +0200

    break y cosas raras
    break y cosas raras añadidas ahí to wapo me mato jajj

--- calculadoramadeinChina/calculadoramadeinChina/Program.cs
+++ calculadoramadeinChina/calculadoramadeinChina/Program.cs
@@ -4,16 +4,31 @@ namespace calculadoramadeinChina
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void ManageMainMenu()
         {
             while (true)
             {
                 UserInterface.PrintMainMenu();
-                string option = System.Console.ReadLine();
-                System.Console.WriteLine("Has elegido " + option);
-
+                int option = UserInterface.ReadOption();
+                if (option == 0)
+                    break;
+                else if (option == -1)
+                {
+                    System.Console.Clear();
+                    System.Console.WriteLine("Opción no válida");
+                }
+                else if (option < 0 || option > 7)
+                {
+                    System.Console.Clear();
+                    System.Console.WriteLine("Opción no válida, introduce un número entre el 0 y el 7");
+                }         
             }
-            
+
         }
+        static void Main(string[] args)
+        {
+            ManageMainMenu();
+        }
+        
     }
 }
--- calculadoramadeinChina/calculadoramadeinChina/UserInterface.cs
+++ calculadoramadeinChina/calculadoramadeinChina/UserInterface.cs
@@ -18,7 +18,31 @@ namespace calculadoramadeinChina
             System.Console.WriteLine("6) Comprobar si un número es primo");
             System.Console.WriteLine("7) Calcular sumatorio de un número");
             System.Console.WriteLine("0) Salir de la aplicación");
+        }
 
+        public static int ReadOption()
+        {
+            string option = System.Console.ReadLine();
+            try
+            {
+                int result = System.Convert.ToInt32(option);
+                return result;
+            }
+
+            catch (System.Exception e)
+            {
+                return -1;
+            }
+
+            //finally
+            //{
+            //
+            //}
+
+            // int result = System.Int32.Parse(option);
+            // int op1 = int.Parse(option);
+            //int result = System.Convert.ToInt32(option);
+            //return result;
 
         }
     }

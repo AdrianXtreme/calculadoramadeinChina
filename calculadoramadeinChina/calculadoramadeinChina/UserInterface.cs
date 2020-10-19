305774344b98015a7e617bd2e7e60cfe5e6415b3
AdrianXtreme <71661344+AdrianXtreme@users.noreply.github.com>
2020-10-19 17:32:09 +0200

    mainmenu
    implemented printmainmenu funtion

---
+++ calculadoramadeinChina/calculadoramadeinChina/UserInterface.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace calculadoramadeinChina
+{
+    class UserInterface
+    {
+        public static void PrintMainMenu()
+        {
+            System.Console.WriteLine("       Calculadora Made in China       ");
+            System.Console.WriteLine("---------------------------------------");
+            System.Console.WriteLine("1) Sumar una serie de números");
+            System.Console.WriteLine("2) Restar una serie de números");
+            System.Console.WriteLine("3) Multiplicar una serie de números");
+            System.Console.WriteLine("4) Dividir una serie de números");
+            System.Console.WriteLine("5) Calcular raíz cuadrada de un número");
+            System.Console.WriteLine("6) Comprobar si un número es primo");
+            System.Console.WriteLine("7) Calcular sumatorio de un número");
+            System.Console.WriteLine("0) Salir de la aplicación");
+
+
+        }
+    }
+}
